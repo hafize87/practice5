ba626989790d6b4b9d8cf84a2ff741e53df3b1e6
Hafize Aksoy <hafize.teacher@gmail.com>
2021-06-30 15:59:34 -0700

    commit new branch

--- src/test/java/A.java
+++ src/test/java/A.java
@@ -6,6 +6,7 @@ public class A {
         System.out.println("commit2");
         System.out.println("commit1 from git");
         System.out.println("commit2 from git");
+        System.out.println("New commit from branch");
         
     }
 }
